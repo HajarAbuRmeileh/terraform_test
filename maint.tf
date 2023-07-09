6b5fdd0f4f262ab6ee9f8c0a0888fbfee2ef4f3c
HajarAbuRmeileh <hajar.maint@gmail.com>
2023-07-10 00:44:04 +0300

    main.tf

--- maint.tf
+++ maint.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = ">= 2.46.0, >= 3.11.0, >= 3.27.0, < 4.0.0"
     }
   }
 
