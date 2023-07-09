c24349983ee1bbc0cd022f41f68f8245e0d4d427
HajarAbuRmeileh <hajar.maint@gmail.com>
2023-07-10 00:02:25 +0300

    main.tf

---
+++ maint.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+
+ backend "azurerm" {
+    resource_group_name  = "Terraform"
+    storage_account_name = "terraform4storage"
+    container_name       = "tfstatefile"
+     key                 = "dev.Terraform.tfstatefile"
+    
+}
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
+}
+module "aks_example_named_cluster" {
+  source  = "Azure/aks/azurerm//examples/named_cluster"
+  version = "6.2.0"
+}
