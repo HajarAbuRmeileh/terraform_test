50f82a02d2f1b245076ecf71384b251132147fb3
HajarAbuRmeileh <hajar.maint@gmail.com>
2023-07-15 16:40:31 +0300

    first commit

--- infra/main.tf
+++ infra/main.tf
@@ -1,95 +1,45 @@
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = var.cluster_name
   location            = azurerm_resource_group.rg.location
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = var.dns_prefix
-  kubernetes_version  = var.kubernetes_version
-
-  default_node_pool {
-    name       = "systempool"
-    node_count = var.node_count
-    vm_size    = "Standard_D2_v2"
-  }
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
 
   identity {
     type = "SystemAssigned"
   }
 
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.node_count
+  }
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+    }
+  }
   network_profile {
-    network_plugin = "kubenet"
-    network_policy = "calico"
-  }
-
-  tags = {
-    Environment = "Development"
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
   }
 }
-
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
-  admin_enabled       = true # false
-}
-
-data "azurerm_client_config" "current" {
-}
-
-data "azurerm_user_assigned_identity" "identity" {
-  name                = "${azurerm_kubernetes_cluster.k8s.name}-agentpool"
-  resource_group_name = azurerm_kubernetes_cluster.k8s.node_resource_group
-}
-
-#resource "azurerm_role_assignment" "role_acrpull" {
-  #scope                            = azurerm_container_registry.acr.id
-  #role_definition_name             = "AcrPull"
-  #principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
-  #skip_service_principal_aad_check = true
-#}
-
-#not ness
-# resource "azurerm_sql_server" "sql" {
-#   name                         = var.sql_name
-#   resource_group_name          = azurerm_resource_group.rg.name
-#   location                     = azurerm_resource_group.rg.location
-#   version                      = "12.0"
-#   administrator_login          = var.db_admin_login
-#   administrator_login_password = var.db_admin_password
-# }
-# 
-# resource "azurerm_storage_account" "storage" {
-#   name                     = var.storage_name
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
-# 
-# resource "azurerm_sql_database" "db" {
-#   name                = var.db_name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = azurerm_resource_group.rg.location
-#   server_name         = azurerm_sql_server.sql.name
-#   create_mode         = "Default"
-#   edition             = "Standard"
-# 
-#   tags = {
-#     environment = "production"
-#   }
-# }
-# 
-# resource "azurerm_sql_firewall_rule" "rule" {
-#   name                = "AllowAzureServicesAndResources"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   server_name         = azurerm_sql_server.sql.name
-#   # The Azure feature Allow access to Azure services can be enabled 
-#   # by setting start_ip_address and end_ip_address to 0.0.0.0
-#   start_ip_address = "0.0.0.0"
-#   end_ip_address   = "0.0.0.0"
-# }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,47 +1,23 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
 variable "node_count" {
-  default = 1
+  type        = number
+  description = "The initial quantity of nodes for the node pool."
+  default     = 1
 }
 
-variable "dns_prefix" {
-  default = "aks-k8s-2022"
-}
-
-variable "cluster_name" {
-  default = "aks-k8s-2022"
-}
-
-variable "kubernetes_version" {
-  default = "1.24.10"
-}
-
-variable "acr_name" {
-  default = "acrforaks2022"
-}
-
-variable "sql_name" {
-  default = "mssql-2022"
-}
-
-variable "db_name" {
-  default = "ProductsDB"
-}
-
-variable "db_admin_login" {
-  default = "houssem"
-}
-
-variable "db_admin_password" {
-  default = "@Aa123456"
-}
-
-variable "storage_name" {
-  default = "mssqlstorageaccount2022"
-}
-
-variable "resource_group_name" {
-  default = "aks-k8s-2020"
-}
-
-variable "location" {
-  default = "eastus"
+variable "msi_id" {
+  type        = string
+  description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
+  default     = null
 }
